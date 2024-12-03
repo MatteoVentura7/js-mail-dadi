40cbab5dd500977afb3da7b30fc8410843c387f1
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-03 16:11:09 +0100

    add : 01-mail

--- 01-mail/script.js
+++ 01-mail/script.js
@@ -1,1 +1,28 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+const ListMail = [
+    'matteo@libero.it',
+    'luca@gmail.com',
+    'Fabio@gmail.com',
+    'Phil@outlook.it',
+    'Carlo@boolean.com',
+  ];
+
+  // 1 - Chiedo la mail all'utente
+const MailUtente =(prompt("Inserisci la tua Mail"));
+
+let trovato = false;
+
+// Controllo usando un ciclo for
+for (let i = 0; i < ListMail.length; i++) {
+    if (ListMail[i] === MailUtente) {
+        trovato = true;
+        break;
+    }
+}
+
+if (trovato) {
+    alert(`Mail corretta`);
+} else {
+    alert(`Errore! Mail non valida`);
+}
